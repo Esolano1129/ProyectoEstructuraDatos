d8b593cd4c6828d3182a1402db324c09a5a5d0d9
Esolano1129 <edgardoasolano@gmail.com>
2025-02-23 20:35:57 -0600

    commit prueba 2

--- src/main/java/com/ejercicio1/proyectofinaled/Main.java
+++ src/main/java/com/ejercicio1/proyectofinaled/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        //pruebaa test 2
+      
         Menu menu = new Menu();
         menu.desplegarMenu();
        } 
